04be4d2312129edd4bdfc721f35e086e55d29fb2
guizinnn12 <gui.andrade.corp@gmail.com>
2024-06-14 12:10:44 -0300

    exercicios

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,7 @@
+//Para executar usar Fn + F8
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}Km/Hr`)
+if (vel > 60) { // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
