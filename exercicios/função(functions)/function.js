e6546bc482d3c2f4ed904549f0059373c82ef5ef
guizinnn12 <gui.andrade.corp@gmail.com>
2024-06-25 17:45:37 -0300

    functions

---
+++ exercicios/função(functions)/function.js
@@ -0,0 +1,44 @@
+function dizerNome(nome) {
+
+    console.log(' O nome é: ' + nome)
+}
+
+dizerNome('Guilherme')
+dizerNome('Joaquim')
+dizerNome('Gobato')
+
+function dizerIdade(idade) {
+    
+    console.log('A idade respectiva é: ' + idade)
+}
+
+dizerIdade('18 anos')
+dizerIdade('7 anos')
+dizerIdade('10 meses')
+
+function somar(a, b) {
+    let soma = a + b
+    return soma
+}
+
+let somaUm = somar(2, 10)
+
+console.log('A soma resultou em ' + somaUm)
+
+function multi(a, b) {
+    let multi = a * b
+    return multi
+}
+
+let multiUm = multi(2, 10)
+
+console.log('A multiplicação resultou em ' + multiUm)
+
+function divisao( a, b) {
+    let divisao = a / b
+    return divisao
+}
+
+let divUm = divisao(10, 2 )
+
+console.log('A divisão resultou em ' + divUm)
